37bd5f9059e57c531b9b534b8f71ce5360bb712e
Riyad <74069878+Riyad-boop@users.noreply.github.com>
2020-12-01 23:45:51 +0000

    Multiplayer+Zombies+Map
    riyad + waheed
    (whoever wants to do map i think we need precise box colliders for each object in the scene where we want to reflect it)

---
+++ Assets/Scripts/Scripts_Nuareo Tek/EnemyAI/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public NavMeshAgent Agent;
+    public Transform Player;
+	public GameObject bloodSpray;
+
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        Agent.SetDestination(Player.position);
+    }
+
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			Debug.Log("attack");
+		}
+
+		else if (collision.gameObject.tag == "Bullet")
+		{
+			Debug.Log("enemy dead");
+			Explode();
+		}
+
+	}
+
+	private void Explode()
+	{
+		Quaternion rot = new Quaternion(transform.rotation.x, transform.rotation.y, 90f, transform.rotation.w);
+		
+		Instantiate(bloodSpray, transform.position, bloodSpray.transform.rotation);
+		Destroy(this.gameObject, 0f);
+	}
+
+}
