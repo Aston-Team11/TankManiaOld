9511fefacbb5f8dbed66024d748284694de91235
Bal <190206866@aston.ac.uk>
2020-12-14 01:56:21 +0000

    Added Riyad ver

---
+++ Assets/Scripts/Scripts_Nuareo Tek/EnemyAI/Spawner.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Spawner : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private int waveNumber = 0;
+    [SerializeField] private int enemySpawnAmount = 0;
+    [SerializeField] private int enemiesKilled = 0;
+
+
+    public GameObject[] spawners;
+    public GameObject enemy;
+    public GameObject target;
+    public int Spawncount = 4;
+
+    //Riyad
+    private GameObject player;
+
+    [SerializeField] private List<GameObject> PlayerLists = new List<GameObject>();
+
+
+    public void IncrementEnemies()
+    {
+        enemiesKilled++;
+
+        if (photonView.IsMine){
+            photonView.RPC("SyncDeathCount", RpcTarget.OthersBuffered, enemiesKilled);
+        }
+
+    }
+
+
+    public int getWave()
+    {
+        return waveNumber;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set base vlaues of this class
+        waveNumber = 1;
+        enemySpawnAmount = 5;
+        enemiesKilled = 0;
+
+        spawners = new GameObject[5];
+
+        for(int i = 0; i < spawners.Length; i++)
+        {
+            spawners[i] = transform.GetChild(i).gameObject;
+        }
+
+       
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(photonView.IsMine)) return;
+
+        if(enemiesKilled >= enemySpawnAmount)
+        {
+            NextWave();
+        }
+
+    }
+
+
+    // update stats on everyone's screens
+    [PunRPC]
+     private void SyncWave(int wave)
+      {
+        waveNumber = wave;
+     }
+
+    // update stats on everyone's screens
+    [PunRPC]
+    private void SyncDeathCount(int killed)
+    {
+        enemiesKilled = killed;
+    }
+
+
+
+    private void NextWave()
+    {
+        waveNumber++;
+        enemySpawnAmount += 5;
+        enemiesKilled = 0;
+        photonView.RPC("SyncWave", RpcTarget.OthersBuffered, waveNumber);
+
+        for (int i = 0; i < enemySpawnAmount; i++)
+        {
+            SpawnEnemy();        
+        }
+    }
+
+   
+    private void SpawnEnemy()
+    {
+        Spawncount++;
+        int spawnerID = Random.Range(0, spawners.Length);
+        
+
+        randomiseSelection();
+        var zombie = PhotonNetwork.Instantiate(enemy.name, spawners[spawnerID].transform.position, spawners[spawnerID].transform.rotation);
+
+        zombie.name = "zombie" + Spawncount; // test
+
+
+    }
+
+
+
+    /// <summary>
+    /// @author Riyad K Rahman
+    /// </summary>
+    /// <param name="player"></param>
+    public void addPlayer(GameObject player)
+    {
+        if (!(PlayerLists.Contains(player)))
+        {
+            PlayerLists.Add(player);
+        }
+
+        if (PlayerLists.Count > 1)
+        {
+            PlayerLists.Sort(sortFunction);
+        }
+
+    }
+
+
+    /// <summary>
+    /// @author Riyad K Rahman
+    /// </summary>
+    /// <param name="p1"></param>
+    /// <param name="p2"></param>
+    /// <returns></returns>
+    private int sortFunction(GameObject p1, GameObject p2)
+    {
+        if (p1.GetComponent<PlayerManager>().GetOrder() < p2.GetComponent<PlayerManager>().GetOrder())
+        {
+            return -1;
+        }
+        else if (p1.GetComponent<PlayerManager>().GetOrder() > p2.GetComponent<PlayerManager>().GetOrder())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+
+    /// <summary>
+    /// @Riyad K Rahman
+    /// Choose a random player to target when spawned
+    /// </summary>
+    public void randomiseSelection()
+    {
+        photonView.RPC("targetPlayer", RpcTarget.AllBuffered, Random.Range(0, PlayerLists.Count));
+        //targetPlayer(Random.Range(0, PlayerLists.Count));
+    }
+
+    [PunRPC]
+    private void targetPlayer(int result)
+    {
+        target = PlayerLists[result];
+        enemy.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControl>().getPlayers(target.transform);
+    }
+
+}
---
+++ Assets/Scripts/Scripts_Nuareo Tek/Menu/Manager.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+using UnityEngine.SceneManagement;
+
+
+public class Manager : MonoBehaviourPunCallbacks
+{
+    public string player_prefab;
+    public GameObject playerObject;
+    public string mouseReticle;
+    public string Enemy;
+    public Transform SpawnPoint, SpawnPointEnemy;
+    public GameObject time;
+    public int playerCount = 0;
+    [SerializeField] private int RespawnTime;
+    public int count;
+    [SerializeField] private List<GameObject> PlayerLists = new List<GameObject>();
+
+
+
+    /// <summary>
+    /// @author Riyad K Rahman
+    /// when the game starts spawn players and zombies
+    /// </summary>
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var target = SpawnPlayer();
+        SpawnZombie(target);
+    }
+
+
+
+    /// <summary>
+    /// @author Riyad K Rahman
+    /// spawn player, set ID, assign time object so player can control time when powerup is enabled
+    /// </summary>
+    /// <returns>player transform so that the enemies can find the player </returns>
+    private Transform SpawnPlayer()
+    {
+        photonView.RPC("IncrementPlayerCount", RpcTarget.AllBuffered);
+
+        
+        var player = PhotonNetwork.Instantiate(playerObject.name, SpawnPoint.position, SpawnPoint.rotation);
+
+         if (!(photonView.IsMine))
+         {
+            // set player counts
+            if (GameObject.Find("2") == null )
+            {
+                player.name = "2";
+            }
+
+            else if (GameObject.Find("3") == null)
+            {
+                player.name = "3";
+            }
+
+            else if (GameObject.Find("4") == null)
+            {
+                player.name = "4";
+            }
+
+        }
+         else
+         {
+             player.name = "1";
+         }
+
+        //player.SendMessage("SetOrder",1);
+        // player.SendMessage("setPlayerID", playerCount);
+        player.SendMessage("setTimeObject", time);
+        PlayerLists.Add(player);
+
+        return player.transform;
+    }
+
+    [PunRPC]
+    public void IncrementPlayerCount(){
+        playerCount++;
+
+    }
+
+
+    /// <summary>
+    /// @author Riyad K Rahman
+    /// spawns a zombie and sends the players position
+    /// </summary>
+    /// <param name="targetPosition"> the player 1 posistion in the world</param>
+    private void SpawnZombie(Transform targetPosition)
+    {
+        if (!(photonView.IsMine)) return;
+
+        for (int i = 0; i < 5; i++)
+        {
+            var zombie = PhotonNetwork.Instantiate(Enemy, SpawnPointEnemy.position, SpawnPointEnemy.rotation);
+            zombie.SetActive(true);
+            zombie.name = "zombie" + i;
+            zombie.SendMessage("TargetPlayer1");
+
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="deadPlayer"></param>
+    private void Respawn(GameObject deadPlayer)
+    {
+        //deadPlayer.SetActive(false);
+        //StartCoroutine(RespawnPlayer(deadPlayer));
+        RespawnTime += 10;
+        PlayerLists.Remove(deadPlayer);
+
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="RespawningPlayer"></param>
+    /// <returns></returns>
+    IEnumerator RespawnPlayer(GameObject RespawningPlayer)
+    {
+        //!!!! check player number of alives here? if lives are 0 then dont respawn player 
+        yield return new WaitForSeconds(RespawnTime);
+        RespawningPlayer.GetComponent<PlayerManager>().StatReset();
+        RespawningPlayer.SetActive(true);
+        RespawningPlayer.transform.position = SpawnPoint.position;
+        PlayerLists.Add(RespawningPlayer);
+    }
+
+    public void Update()
+    {
+        if (photonView.IsMine)
+        {
+            //CheckForEndGame();
+            if(PlayerLists.Count <= 0)
+            {
+                photonView.RPC("EndGame", RpcTarget.All);
+                //EndGame();
+            }
+        }
+        
+    }
+
+
+    public void CheckForEndGame()
+    {
+        foreach(GameObject player in PlayerLists)
+        {
+            if(player.activeSelf == false)
+            {
+                count++;
+            }
+
+        }
+
+        if (count.Equals(playerCount)) {
+            //EndGame();
+            photonView.RPC("EndGame", RpcTarget.All);
+        }
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("EndCredits", LoadSceneMode.Single);
+
+    }
+
+}
+
+// if (!(photonView.IsMine))
+// {
+//     Master = GameObject.Find("1");
+//     player.name = "2";
+// }
+// else
+// {
+//     player.name = "1";
+// }
