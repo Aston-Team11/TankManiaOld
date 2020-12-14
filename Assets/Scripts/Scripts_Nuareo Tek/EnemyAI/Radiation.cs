7a4d4536921c1d13bcca2287034e5e0cfd49af4e
Bal <190206866@aston.ac.uk>
2020-12-14 02:58:12 +0000

    Riyad updates

---
+++ Assets/Scripts/Scripts_Nuareo Tek/EnemyAI/Radiation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radiation : MonoBehaviour
+{
+    public float sphereRadius;
+    [SerializeField] private GameObject player;
+   
+
+    public void Start()
+    {
+          StartCoroutine(endAll());
+    }
+
+    IEnumerator endAll()
+    {
+        yield return new WaitForSeconds(9f);
+        Destroy(this.gameObject);
+
+    }
+
+
+    private void OnTriggerEnter(Collider playerCollider)
+    {
+
+        if (playerCollider.gameObject.CompareTag("Player"))
+        {
+            //Debug.LogWarning("AOE Damage works");
+
+            player = playerCollider.gameObject;
+            player.GetComponent<PlayerManager>().Setpoisoned(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider playerCollider)
+    {
+        if (playerCollider.gameObject.CompareTag("Player") )
+        {
+            player = playerCollider.gameObject;
+           player.GetComponent<PlayerManager>().Setpoisoned(false);
+
+        }
+    }
+
+      
+ }
--- Assets/Scripts/Scripts_Nuareo Tek/Menu/Launcher.cs
+++ Assets/Scripts/Scripts_Nuareo Tek/Menu/Launcher.cs
@@ -2,88 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using Photon.Realtime;// To be changed over
-using UnityEngine.SceneManagement;
 
 
-/// <summary>
-/// Basic script handling the connection to a server and creating and joining room.
-/// </summary>
-public class Launcher : MonoBehaviourPunCallbacks
+namespace com.Riyad.TankMania
 {
-    private string roomName = "";
-    static private string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public void Awake()
+    /// <summary>
+    /// basic match making system max 20 ppl
+    /// </summary>
+    public class Launcher : MonoBehaviourPunCallbacks
     {
-        PhotonNetwork.AutomaticallySyncScene = true; // Sets options to always sync the scene to who is the Master client (Host)
-        PhotonNetwork.GameVersion = "1.5.4";
-    }
-
-    public void Start()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    public void OnClickCreate() 
-    {
-        PhotonNetwork.ConnectUsingSettings();  // Connect to server (Settings: APP ID, Region, Server Address) calls OnConnectedToMaster()
-        StartCoroutine(roomCreate()); // Call roomCreate()
-    }
-
-
-     public void OnClickJoin(string roomName)
-     {
-        PhotonNetwork.ConnectUsingSettings();
-        this.roomName = roomName;
-        StartCoroutine(JoinRoom());
-         
-     }
-
-    public IEnumerator roomCreate()
-    {
-        yield return new WaitForSeconds(2); // Waites for one second before calling CreateRoom()
-        CreateRoom();
-    }
-
-    public IEnumerator JoinRoom()
-    {
-        yield return new WaitForSeconds(2); // Waites for one second before calling CreateRoom()
-        PhotonNetwork.JoinRoom(roomName);
-    }
-
-
-    public override void OnJoinedRoom()
-    {
-        StartGame();
-        base.OnJoinedRoom();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void StartGame()
-    {
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 1) // if there is one player in the room, load the level
+        public void Awake()
         {
-            PhotonNetwork.LoadLevel(1);
+            PhotonNetwork.AutomaticallySyncScene = true;
+            Connect();
         }
-    }
 
-    public void CreateRoom()
-    {
-        for (int i = 0; i < 5; i++) //Create a roomName of 5 chars
+        public override void OnConnectedToMaster()
+        { 
+            Join();
+          
+            base.OnConnectedToMaster();
+        }
+
+
+        public override void OnJoinedRoom()
         {
-            roomName += chars[Random.Range(0, chars.Length)];
+            StartGame();
+             base.OnJoinedRoom();
         }
-        Debug.Log(roomName);
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.MaxPlayers = 4; // Set max players in a room to be 4
-        PhotonNetwork.CreateRoom(roomName, roomOptions); // Create a room with Name (str) and roomOptions
-    }
 
-    public string getRoomName() { return roomName; }
-    public void setRoomName(string name) { roomName = name; }
+        //if the join fails
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            CreateMatch();
+            base.OnJoinRandomFailed(returnCode, message);
+        }
+
+        public void Connect() 
+        {
+            PhotonNetwork.GameVersion = "1.5.4";
+            PhotonNetwork.ConnectUsingSettings();
+
+        }
+
+        public void Join()
+        {
+            PhotonNetwork.JoinRandomRoom();                 // uncomment for parallelSync
+            //CreateMatch();                              // Only for single player testing use otherwise uncomment
+
+        }
+
+        public void StartGame()
+        {
+            //if there is exactly one player load the scene
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                PhotonNetwork.LoadLevel(1);
+            }
+        }
+
+        public void CreateMatch()
+        {
+            //PhotonNetwork.CreateRoom("riyad");   // Only for single player testing use otherwise uncomment
+            PhotonNetwork.CreateRoom("");   // remove "riayd" for parallelSync
+        }
+
+
+
+    }
 
 
 }
-
-
